d1dc112612a384d0dda898de4b8e9ce36dd03e65
NielSmyth <danielosezele29@gmail.com>
2025-07-13 12:48:29 +0100

    neww update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "//smart-home-u4h5",
+    base: process.env.VITE_BASE_PATH || "//SmartHome",
 });
